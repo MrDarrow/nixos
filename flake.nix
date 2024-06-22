84d98d82e050a7479e745fdcbdf4b3273939bff3
MrDarrow <mrdarrow@netc.fr>
2024-06-22 12:30:04 -0400

    renamed:    home-manager/flake.lock -> flake.lock 	new file:   flake.nix 	deleted:    home-manager/flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "My system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+
+    let
+
+      system = "x86_64-linux";
+
+    in {
+
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+
+        inherit system;
+
+	modules = [ ./nixos/configuration.nix ];
+
+      };
+
+    homeConfigurations.darrow = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      modules = [ ./home-manager/home.nix ];
+    };
+  };
+
+}
