a18f60c1c62bff443a948322faedf50a30086522
MrDarrow <mrdarrow@netc.fr>
2024-07-19 13:29:57 -0400

    modified:   home-manager/zsh.nix

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -8,7 +8,7 @@
 
     shellAliases =
       let
-        flakeDir = "~/tmp/nixos/";
+        flakeDir = "~/nixos";
       in {
     rb = "sudo nixos-rebuild switch --flake ${flakeDir}";
     upd = "nix flake update ${flakeDir}";
