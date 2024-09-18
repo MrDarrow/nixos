fa19612da43d498262ee8b0d4198aeb85c4a92a6
MrDarrow <mrdarrow@waifu.club>
2024-09-18 17:24:17 -0400

    modified:   binds.nix 	modified:   default.nix 	new file:   exec.nix

--- home-manager/hyprland/binds.nix
+++ home-manager/hyprland/binds.nix
@@ -6,83 +6,80 @@
 
   in
 {
-
-
   wayland.windowManager.hyprland.settings = {
-      bind = [
-        # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-       "$mainMod, RETURN, exec, $terminal"
-       "$mainMod SHIFT, RETURN, exec, [float;size 50%] $terminal"
-       "$mainMod, Q, killactive,"
-       "$mainMod SHIFT, Q, exit,"
-       "$mainMod, U, togglefloating,"
-       "$mainMod SHIFT, F, fullscreen"
-       ''$mainMod SHIFT, Print, exec, grim -g "$(slurp -d)" - | wl-copy''
+    bind = [
+      "${mod}, RETURN, exec, $terminal"
+      "${mod} ${shift}, RETURN, exec, [float;size 50%] $terminal"
+      "${mod}, Q, killactive,"
+      "${mod} ${shift}, Q, exit,"
+      "${mod}, U, togglefloating,"
+      "${mod} ${shift}, F, fullscreen"
+      ''${mod} ${shift}, Print, exec, grim -g "$(slurp -d)" - | wl-copy''
 
-        # Navigate
-       "$mainMod, J, layoutmsg, cyclenext"
-       "$mainMod, K, layoutmsg, cycleprev"
-       "$mainMod SHIFT, J, layoutmsg, swapnext"
-       "$mainMod SHIFT, K, layoutmsg, swapprev"
+       # Navigate
+      "${mod}, J, layoutmsg, cyclenext"
+      "${mod}, K, layoutmsg, cycleprev"
+      "${mod} ${shift}, J, layoutmsg, swapnext"
+      "${mod} ${shift}, K, layoutmsg, swapprev"
 
-        # Programs
-       "$mainMod, W, exec, librewolf"
-       "$mainMod, D, exec, freetube"
-       "$mainMod, P, exec, rofi -show drun -show-icons"
-       "$mainMod, F, exec, $terminal yazi"
-       "$mainMod, N, exec, $terminal newsboat"
+       # Programs
+      "${mod}, W, exec, librewolf"
+      "${mod}, D, exec, freetube"
+      "${mod}, P, exec, rofi -show drun -show-icons"
+      "${mod}, F, exec, $terminal yazi"
+      "${mod}, N, exec, $terminal newsboat"
 
 
-        # Move focus with mainMod + arrow keys
-       "$mainMod, left, movefocus, l"
-       "$mainMod, right, movefocus, r"
-       "$mainMod, up, movefocus, u"
-       "$mainMod, down, movefocus, d"
+       # Move focus with mainMod + arrow keys
+      "${mod}, left, movefocus, l"
+      "${mod}, right, movefocus, r"
+      "${mod}, up, movefocus, u"
+      "${mod}, down, movefocus, d"
 
-        # Switch workspaces with mainMod + [0-9]
-       "$mainMod, 1, workspace, 1"
-       "$mainMod, 2, workspace, 2"
-       "$mainMod, 3, workspace, 3"
-       "$mainMod, 4, workspace, 4"
-       "$mainMod, 5, workspace, 5"
-       "$mainMod, 6, workspace, 6"
-       "$mainMod, 7, workspace, 7"
-       "$mainMod, 8, workspace, 8"
-       "$mainMod, 9, workspace, 9"
-       "$mainMod, 0, workspace, 10"
+       # Switch workspaces with mainMod + [0-9]
+      "${mod}, 1, workspace, 1"
+      "${mod}, 2, workspace, 2"
+      "${mod}, 3, workspace, 3"
+      "${mod}, 4, workspace, 4"
+      "${mod}, 5, workspace, 5"
+      "${mod}, 6, workspace, 6"
+      "${mod}, 7, workspace, 7"
+      "${mod}, 8, workspace, 8"
+      "${mod}, 9, workspace, 9"
+      "${mod}, 0, workspace, 10"
 
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]       
-       "$mainMod SHIFT, 1, movetoworkspace, 1"
-       "$mainMod SHIFT, 2, movetoworkspace, 2"
-       "$mainMod SHIFT, 3, movetoworkspace, 3"
-       "$mainMod SHIFT, 4, movetoworkspace, 4"
-       "$mainMod SHIFT, 5, movetoworkspace, 5"
-       "$mainMod SHIFT, 6, movetoworkspace, 6"
-       "$mainMod SHIFT, 7, movetoworkspace, 7"
-       "$mainMod SHIFT, 8, movetoworkspace, 8"
-       "$mainMod SHIFT, 9, movetoworkspace, 9"
-       "$mainMod SHIFT, 0, movetoworkspace, 10"
+       # Move active window to a workspace with mainMod + ${shift} + [0-9]       
+      "${mod} ${shift}, 1, movetoworkspace, 1"
+      "${mod} ${shift}, 2, movetoworkspace, 2"
+      "${mod} ${shift}, 3, movetoworkspace, 3"
+      "${mod} ${shift}, 4, movetoworkspace, 4"
+      "${mod} ${shift}, 5, movetoworkspace, 5"
+      "${mod} ${shift}, 6, movetoworkspace, 6"
+      "${mod} ${shift}, 7, movetoworkspace, 7"
+      "${mod} ${shift}, 8, movetoworkspace, 8"
+      "${mod} ${shift}, 9, movetoworkspace, 9"
+      "${mod} ${shift}, 0, movetoworkspace, 10"
 
-       "ALT SHIFT, 1, movetoworkspacesilent, 1"
-       "ALT SHIFT, 2, movetoworkspacesilent, 2"
-       "ALT SHIFT, 3, movetoworkspacesilent, 3"
-       "ALT SHIFT, 4, movetoworkspacesilent, 4"
-       "ALT SHIFT, 5, movetoworkspacesilent, 5"
-       "ALT SHIFT, 6, movetoworkspacesilent, 6"
-       "ALT SHIFT, 7, movetoworkspacesilent, 7"
-       "ALT SHIFT, 8, movetoworkspacesilent, 8"
-       "ALT SHIFT, 9, movetoworkspacesilent, 9"
-       "ALT SHIFT, 0, movetoworkspacesilent, 10"
+      "${alt} ${shift}, 1, movetoworkspacesilent, 1"
+      "${alt} ${shift}, 2, movetoworkspacesilent, 2"
+      "${alt} ${shift}, 3, movetoworkspacesilent, 3"
+      "${alt} ${shift}, 4, movetoworkspacesilent, 4"
+      "${alt} ${shift}, 5, movetoworkspacesilent, 5"
+      "${alt} ${shift}, 6, movetoworkspacesilent, 6"
+      "${alt} ${shift}, 7, movetoworkspacesilent, 7"
+      "${alt} ${shift}, 8, movetoworkspacesilent, 8"
+      "${alt} ${shift}, 9, movetoworkspacesilent, 9"
+      "${alt} ${shift}, 0, movetoworkspacesilent, 10"
 
-        # Scroll through existing workspaces with mainMod + scroll
-       "$mainMod, mouse_down, workspace, e+1"
-       "$mainMod, mouse_up, workspace, e-1"
-      ];
+       # Scroll through existing workspaces with mainMod + scroll
+      "${mod}, mouse_down, workspace, e+1"
+      "${mod}, mouse_up, workspace, e-1"
+     ];
 
-      # Move/resize windows with mainMod + LMB/RMB and dragging
-      bindm = [
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
-      ];
+     # Move/resize windows with mainMod + LMB/RMB and dragging
+     bindm = [
+       "${mod}, mouse:272, movewindow"
+       "${mod}, mouse:273, resizewindow"
+     ];
   };
 }
