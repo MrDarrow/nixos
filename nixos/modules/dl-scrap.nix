8c124aa9eb4da87eb8aedede9e1aac8981161e6e
MrDarrow <mrdarrow@waifu.club>
2024-09-18 08:07:41 -0400

    deleted:    ../../home-manager/modules/programs/yazi.nix 	new file:   dl-scrap.nix

---
+++ nixos/modules/dl-scrap.nix
@@ -0,0 +1,15 @@
+{ pkgs }:
+
+{
+  pkgs.writeShellScriptBin "dl-scrap" ''
+  
+  ${pkgs.wget}/bin/wget "https://www.dlsite.com/maniax/work/=/product_id/$1.html" -P /tmp
+
+  url = "$(grep img.dlsite.jp/modpub /tmp/$1.html | grep -o '".*"' | sed 's\"\\g' | grep -o 'https.*' | head -1)"
+
+  rm /tmp/$1.html
+
+  ${pkgs.wget}/bin/wget $url
+  ${pkgs.wget}/bin/wget "$(echo $url | sed 's/_main/_sam_mimi/')"
+  ''
+}
